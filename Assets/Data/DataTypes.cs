310b5c9d878e6df4f52aada83213c1b073f70651
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2024-05-17 01:47:17 +0200

    :zap: Switch to Universal 2D & Basic scripts

---
+++ Assets/Data/DataTypes.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+
+namespace Data
+{
+    #region Structs
+
+    // Put your Structs here
+
+    #endregion
+
+
+    #region Enums
+
+    // Put your Enums here
+
+    #endregion
+
+
+    #region Flags
+
+    // Put your Flags here
+
+    #endregion
+}
